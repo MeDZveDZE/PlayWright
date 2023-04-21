b1d83ac0d6a616f8c76a6b7d509b787662022dc8
Vadim Khorava <vadim.khorava@gmail.com>
2023-04-21 15:03:14 +0200

    task 3 version for check

---
+++ Task3/tests/task3.spec.js
@@ -0,0 +1,88 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+import * as loginData from "../files/user2.json";
+
+//авторизация из файла
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://demoqa.com/login");
+  await expect(page.locator(".main-header")).toHaveText("Login");
+  await page.getByPlaceholder("UserName").fill(loginData.userName);
+  await page.getByPlaceholder("Password").fill(loginData.password);
+  await page.locator("#login").click();
+  await expect(page.locator(".main-header")).toHaveText("Profile");
+});
+
+test("main", async ({ page, request }) => {
+  const cookiesData = await page.context().cookies();
+  //по какой-то причине подчёркивает всё красным (c и без await, без разницы). только ts ingore помогает, и тест работает норм
+  // @ts-ignore
+  expect(cookiesData.find((c) => c.name == "userID").value).toBeTruthy();
+  // @ts-ignore
+  expect(cookiesData.find((c) => c.name == "userName").value).toBeTruthy();
+  // @ts-ignore
+  expect(cookiesData.find((c) => c.name == "expires").value).toBeTruthy();
+  // @ts-ignore
+  expect(cookiesData.find((c) => c.name == "token").value).toBeTruthy();
+  // @ts-ignore
+  const userID = cookiesData.find((c) => c.name == "userID").value; //сохранённые userID и token
+  // @ts-ignore
+  const token = cookiesData.find((c) => c.name == "token").value;
+
+//заблокировать все картинки
+  await page.route("**/*", (route) => {
+    return route.request().resourceType() === "image"
+      ? route.abort()
+      : route.continue();
+  });
+  const responsePromise = page.waitForResponse(
+    "https://demoqa.com/BookStore/v1/Books"
+  );
+  await page.locator('.text:text-is("Book Store")').click();
+  //сделать скриншот
+  await page.waitForSelector('.action-buttons');
+  await page.screenshot({ path: "files/screenshot.png" });
+  const response = await responsePromise;
+  await expect(response.status()).toBe(200);
+  const responseBody = await response.json();
+  //check if books in response body = books in UI (.action-buttons locator)
+  await expect(page.locator(".action-buttons")).toHaveCount(
+    responseBody.books.length
+  );
+  //заменить количество страниц на 999
+  await page.route(
+    "https://demoqa.com/BookStore/v1/Book?ISBN=*",
+    async (route) => {
+      const response = await route.fetch();
+      let body = await response.text();
+      const searchBody = JSON.parse(body);
+      body = body.replace(searchBody.pages, "999");
+      route.fulfill({
+        response,
+        body,
+        headers: {
+          ...response.headers(),
+        },
+      });
+    }
+  );
+  await page.pause();
+  //кликнуть на рандомную книгу в списке
+  await page
+    .locator(".action-buttons")
+    .nth(Math.floor(Math.random() * (responseBody.books.length - 1)))
+    .click();
+  await expect(page.locator('#pages-wrapper #userName-value')).toHaveText('999');
+
+  //API test
+  const responseAPI = await request.get(`https://demoqa.com/Account/v1/User/${userID}`, {
+      headers:{
+        'Authorization' : 'Bearer '+ token
+      },
+  }); 
+  const responseApiJson = await responseAPI.json();
+  expect(responseAPI.status()).toBe(200);
+  expect(responseApiJson.books.length).toBe(0);
+  expect(responseApiJson.books).toStrictEqual([]);//такой вариант норм? с toBe([]) выдаёт ошибку. на всякий случай вверху проверка с длинной)
+  expect(responseApiJson.username).toBe(loginData.userName);
+});
