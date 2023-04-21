11427595936db554a66fdffb974b948724c0097f
Vadim Khorava <vadim.khorava@gmail.com>
2023-04-21 12:48:58 +0200

    task 3 2nd

--- Task3/tests/example.spec.js
+++ Task3/tests/example.spec.js
@@ -14,6 +14,7 @@ test.beforeEach(async ({ page }) => {
 
 test('cookies', async ({ page }) => {
   const cookiesData = await page.context().cookies();
+  //по какой-то причине подчёркивает всё красным (c и без await, без разницы). только ts ingore помогает, и тест работает норм
   // @ts-ignore
   expect(cookiesData.find(c => c.name == 'userID').value).toBeTruthy();
   // @ts-ignore
@@ -37,9 +38,36 @@ test ('blockImages', async ({ page }) => {
 })
 const responsePromise = page.waitForResponse('https://demoqa.com/BookStore/v1/Books');
 await page.locator('.text:text-is("Book Store")').click();
-const response = await responsePromise;
 await page.screenshot({path: 'files/screenshot.png'});
+const response = await responsePromise;
+await expect(response.status()).toBe(200);
+const responseBody = await response.json();
+//check if books in response body = books in UI (.action-buttons locator)
+await expect(page.locator('.action-buttons')).toHaveCount(responseBody.books.length);
 
-await expect(page.locator('.action-buttons')).toHaveCount(8);
-console.log(response);
+
+await page.route('https://demoqa.com/BookStore/v1/Book?ISBN=*', async route => {
+  const response = await route.fetch();
+  let body = await response.text();
+  const searchBody = JSON.parse(body);
+  body = body.replace(searchBody.pages, '999');
+  route.fulfill({
+    response,
+    body,
+    headers: {
+      ...response.headers()
+    }
+  });
 });
+await page.pause();
+await page.locator('.action-buttons').nth(2).click();
+
+
+// const responsePromiseBook = page.waitForResponse('https://demoqa.com/BookStore/v1/Book?ISBN=*');
+// await page.locator('.action-buttons').nth(2).click();
+// const responseBook = await responsePromiseBook;
+// const responseBodyBook = await responseBook.text();
+// console.log(responseBodyBook);
+
+});
+
